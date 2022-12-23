879a7a0177ff25b92007c505bb573b7577839bb7
petr <peta.kalina@gmai.com>
2022-12-23 14:00:11 +0100

    release 5.29.1-10

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ project = u'Smooth Archive - DICOM Conformance Statement'
 copyright = u'2022, J4Care GmBH'
 author = u'J4Care GMbH'
 version = u'5.29.1'
-release = u'5.29.1-5'
+release = u'5.29.1-10'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
