885dc59b6dc205252d17ee676cfcf73db6aa5546
petr kalina <peta.kalina@gmail.com>
2026-05-15 21:42:47 -0600

    release 5.35.0-2

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,8 @@ import shlex
 project = u'Smooth Archive - DICOM Conformance Statement'
 copyright = u'2026, J4Care GmbH'
 author = u'J4Care GMbH'
-version = u'5.34.3'
-release = u'5.34.3-5'
+version = u'5.35.0'
+release = u'5.35.0-2'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
